11a78b9b4a768a32180bdba9c918fb1d4dc6ccd0
Omar Villanueva Sotelo <yanhezv@gmail.com>
2024-08-10 10:43:00 -0500

    feat: users controller basic implementation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController { }
+export class UsersController {
+
+  @Get() // GET /users or /users?role=value
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return []
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id }
+  }
+
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate }
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return { id }
+  }
+}
